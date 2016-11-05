22346c2948991727869e7793c2b03578ef945381
PhuongTrang013 <phuongtrang1397@gmail.com>
2016-11-05 23:47:25 +0700

    sua diem dat cua nguoi choi 2 ra giua

--- k21tponggame/vn/vanlanguni/ponggame/PongPanel.java
+++ k21tponggame/vn/vanlanguni/ponggame/PongPanel.java
@@ -257,7 +257,7 @@ public class PongPanel extends JPanel implements ActionListener, KeyListener {
 			g.setColor(Color.blue);
 			g.setFont(new Font(Font.DIALOG, Font.BOLD, 36));
 			g.drawString(String.valueOf(playerOneScore), 100, 100);
-			g.drawString(String.valueOf(playerTwoScore), 400, 100);
+			g.drawString(String.valueOf(playerTwoScore), 350, 100);
 
 			// Draw the winner name
 			g.setFont(new Font(Font.DIALOG, Font.BOLD, 36));

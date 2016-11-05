e0f5120cd6260619388b9bb440a4aac99d36be02
PhuongTrang013 <phuongtrang1397@gmail.com>
2016-11-05 20:37:28 +0700

    Sửa màu đường phân cách 2 người chơi

--- k21tponggame/vn/vanlanguni/ponggame/PongPanel.java
+++ k21tponggame/vn/vanlanguni/ponggame/PongPanel.java
@@ -223,11 +223,13 @@ public class PongPanel extends JPanel implements ActionListener, KeyListener {
 			int playerTwoLeft = playerTwoX;
 
 			// draw dashed line down center
+			g.setColor(Color.GREEN);
 			for (int lineY = 0; lineY < getHeight(); lineY += 50) {
 				g.drawLine(250, lineY, 250, lineY + 25);
 			}
 
 			// draw "goal lines" on each side
+			g.setColor(Color.GRAY);
 			g.drawLine(playerOneRight, 0, playerOneRight, getHeight());
 			g.drawLine(playerTwoLeft, 0, playerTwoLeft, getHeight());
 
